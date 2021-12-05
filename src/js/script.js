c42d32350679c5d172e2b3eadd2dc5688600e265
Yury Kalenkou <goldyukol12@gmail.com>
2021-12-05 12:30:41 +0300

    feat: added slick-slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,29 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const menu = document.querySelector(".header"),
-    hamburger = document.querySelector(".header__hamburger");
+	const menu = document.querySelector(".header"),
+		hamburger = document.querySelector(".header__hamburger");
 
-  hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("header__hamburger_active");
-    menu.classList.toggle("header_active");
-  });
+	hamburger.addEventListener("click", () => {
+		hamburger.classList.toggle("header__hamburger_active");
+		menu.classList.toggle("header_active");
+	});
+});
+
+$(document).ready(function () {
+	$(".carousel__inner").slick({
+		speed: 1200,
+		// adaptiveHeight: true,
+		prevArrow:
+      "<button type=\"button\" class=\"slick-prev\"><img src=\"icons/left-arrow.png\" /></button>",
+		nextArrow:
+      "<button type=\"button\" class=\"slick-next\"><img src=\"icons/right-arrow.png\" /></button>",
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					dots: true,
+					arrows: false,
+				},
+			},
+		],
+	});
 });
