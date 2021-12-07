a91711840708d69bec202a0442a17a184e7224e7
Yury Kalenkou <goldyukol12@gmail.com>
2021-12-07 13:35:36 +0300

    feat: added modals

--- src/js/script.js
+++ src/js/script.js
@@ -57,4 +57,21 @@ $(document).ready(function () {
 
   toggleSlide(".catalog-item__link");
   toggleSlide(".catalog-item__back");
+
+  // Modals
+
+  $("[data-modal=consultation]").on("click", function () {
+    $(".overlay, #consultation").fadeIn("slow");
+  });
+
+  $(".modal__close").on("click", function () {
+    $(".overlay, #consultation, #thanks, #order").fadeOut("slow");
+  });
+
+  $(".button_mini").each(function (i) {
+    $(this).on("click", function () {
+      $("#order .modal__descr").text($(".catalog-item__subtitle").eq(i).text());
+      $(".overlay, #order").fadeIn("slow");
+    });
+  });
 });
